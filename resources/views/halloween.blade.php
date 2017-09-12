3d609fc4a6cfeb24d3c2149b6bdc5735ef639bf0
Leo <leonoragilmour@sky.com>
2017-09-12 18:56:34 +0100

    updating date saver

--- resources/views/halloween.blade.php
+++ resources/views/halloween.blade.php
@@ -7,6 +7,5 @@
 <h2>Save the date for</h2>  
 <h1>HALLOWE'EN</h1>  
 <h2>4th November 2017</h2>
-<h2>Winslow MK18 3HL</h2>
-<p>More details and theme to follow. <br><br>Please let me know asap if you cannot make it.</p>
+<h2>Winslow</h2>
 @stop
