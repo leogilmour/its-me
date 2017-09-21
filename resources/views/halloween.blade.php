e89060035e434c29e955abcc6ec5d33502eb78ab
Leo <leonoragilmour@sky.com>
2017-09-21 09:36:25 +0100

    clarity

--- resources/views/halloween.blade.php
+++ resources/views/halloween.blade.php
@@ -62,7 +62,7 @@
 <h1>R.S.V.P.</h1>
 	<h2>HELL YEAH</h2>
 	<p>The correct choice. Instead of BYOB, I am collecting £10.</p>
-	<p>Have your debit card ready and cough up here:</p>
+	<p>Have your debit card ready (any bank card works) and cough up here:</p>
 	<a href="https://monzo.me/leonoragilmour/10.00?d=From:%20your%20name" class="button">MONZO</a>
 	<p>Once I've received your payment, I will contact you with the unlocked guest list where you can choose your alias and fill out the RSVP form.</p>
 	<p>Preview the guest list here:</p>
