ec77448edd1fe66e0b15d45d067e295a21f146df
Leo <leonoragilmour@sky.com>
2019-02-15 17:53:31 +0000

    just fact view

--- resources/views/wildcard/party.blade.php
+++ resources/views/wildcard/party.blade.php
@@ -8,10 +8,10 @@
 <h3>Attendees</h3>
 @foreach ($guests as $g)
     <div class="guest">
-        <div class="id"><p>{{ $g['guest']->id }}</p></div>
-        <div class="name"><p>{{ $g['guest']->name }} &</p></div>
-        <div class="plus-one-id"><p>{{ $g['plus_one']->id }}</p></div>
-        <div class="plus_one"><p>{{ $g['plus_one']->name }}</p></div>
+        {{-- <div class="id"><p>{{ $g['guest']->id }}</p></div> --}}
+        <div class="name"><p>{{ $g['guest']->name }}</p></div>
+        {{-- <div class="plus-one-id"><p>{{ $g['plus_one']->id }}</p></div> --}}
+        {{-- <div class="plus_one"><p>{{ $g['plus_one']->name }}</p></div> --}}
         <div class="fact">
             @if (isset($g['fact']->cupid_fact))
                 <p>- {{ $g['fact']->cupid_fact }}</p>
@@ -22,10 +22,10 @@
     </div>
 @endforeach
 <h3>Absent</h3>
-@foreach ($absent as $a)
+{{-- @foreach ($absent as $a) --}}
     <div class="absent">
-        <div class="id"><p>{{ $a->id }}</p></div>
-        <div class="name"><p>{{ $a->name }}</p></div>
+        {{-- <div class="id"><p>{{ $a->id }}</p></div>
+        <div class="name"><p>{{ $a->name }}</p></div> --}}
     </div>
-@endforeach
+{{-- @endforeach --}}
 @stop
