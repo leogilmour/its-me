d905d4b1c9ce2bd75d609ccf18216e7a6509e535
Leo Gilmour <leonoragilmour@sky.com>
2021-09-15 20:12:19 +0100

    old laravel

--- storage/framework/views/6c8f1252ef2a66b33073e7c92aa0c1a7973907cf.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-    
-    <p>I'm Leo, a web developer based in London, working for <a href="https://www.stylist.co.uk/visible-women/women-in-tech-development-website-shortlist-media-gender-diversity/212864">Stylist magazine</a>.</p>
-    <p>I tutor in Maths, coding and Science.</p>
-    
-    <p><a href="https://www.instagram.com/themathematicalmermaid/">Instagram</a></p>
-    <p><a href="https://www.linkedin.com/in/leo-gilmour">Linkedin</a></p>
-</div>
